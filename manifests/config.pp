5e2fb6b743d933689434a781dd0652b276b141f1
Mark Heiges <mheiges@uga.edu>
2015-09-29 23:07:05 -0400

    allow more configurations in conf template

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,19 +1,42 @@
 class globus::config(
-  $globus_user = $globus::params::globus_user,
-  $globus_password = $globus::params::globus_password,
-  $endpoint_name = $globus::params::endpoint_name,
-  $endpoint_public = $globus::params::endpoint_public,
-  $endpoint_defaultdirectory = $globus::params::endpoint_defaultdirectory,
-  $security_fetchcredentialfromrelay = $globus::params::security_fetchcredentialfromrelay,
-  $security_identitymethod = $globus::params::security_identitymethod,
-  $gridftp_server = $globus::params::gridftp_server,
-  $myproxy_server = $globus::params::myproxy_server,
-  $oauth_logo = undef,
+  $globus_password                      = $globus::params::globus_password,
+  $globus_user                          = $globus::params::globus_user,
+  $endpoint_defaultdirectory            = $globus::params::endpoint_defaultdirectory,
+  $endpoint_name                        = $globus::params::endpoint_name,
+  $endpoint_public                      = $globus::params::endpoint_public,
+  $security_authorizationmethod         = $globus::params::security_authorizationmethod,
+  $security_certificatefile             = $globus::params::security_certificatefile,
+  $security_cilogonidentityprovider     = $globus::params::security_cilogonidentityprovider,
+  $security_fetchcredentialfromrelay    = $globus::params::security_fetchcredentialfromrelay,
+  $security_gridmap                     = $globus::params::security_gridmap,
+  $security_identitymethod              = $globus::params::security_identitymethod,
+  $security_keyfile                     = $globus::params::security_keyfile,
+  $security_trustedcertificatedirectory = $globus::params::security_trustedcertificatedirectory,
+  $gridftp_datainterface                = $globus::params::gridftp_datainterface,
+  $gridftp_incomingportrange            = $globus::params::gridftp_incomingportrange,
+  $gridftp_outgoingportrange            = $globus::params::gridftp_outgoingportrange,
+  $gridftp_restrictpaths                = $globus::params::gridftp_restrictpaths,
+  $gridftp_server                       = $globus::params::gridftp_server,
+  $gridftp_serverbehindnat              = $globus::params::gridftp_serverbehindnat,
+  $gridftp_sharing                      = $globus::params::gridftp_sharing,
+  $gridftp_sharinggroupsallow           = $globus::params::gridftp_sharinggroupsallow,
+  $gridftp_sharinggroupsdeny            = $globus::params::gridftp_sharinggroupsdeny,
+  $gridftp_sharingrestrictpaths         = $globus::params::gridftp_sharingrestrictpaths,
+  $gridftp_sharingstatedir              = $globus::params::gridftp_sharingstatedir,
+  $gridftp_sharingusersallow            = $globus::params::gridftp_sharingusersallow,
+  $gridftp_sharingusersdeny             = $globus::params::gridftp_sharingusersdeny,
+  $myproxy_cadirectory                  = $globus::params::myproxy_cadirectory,
+  $myproxy_configfile                   = $globus::params::myproxy_configfile,
+  $myproxy_server                       = $globus::params::myproxy_server,
+  $myproxy_serverbehindnat              = $globus::params::myproxy_serverbehindnat,
+  $oauth_logo                           = $globus::params::oauth_logo,
+  $oauth_server                         = $globus::params::oauth_server,
+  $oauth_serverbehindnat                = $globus::params::oauth_serverbehindnat,
 ) {
 
   include globus::params
 
-  $globus_connect_server_conf = $globus::params::globus_connect_server_conf
+  $globus_connect_server_conf    = $globus::params::globus_connect_server_conf
   $globus_connect_server_package = $globus::params::globus_connect_server_package
 
   file { $globus_connect_server_conf:
--- manifests/params.pp
+++ manifests/params.pp
@@ -14,15 +14,39 @@ class globus::params {
       #   [Globus]
       #   User
       # is $globus_user
-      $globus_user   = '%(GLOBUS_USER)s'
-      $globus_password = '%(GLOBUS_PASSWORD)s'
-      $endpoint_name = '%(SHORT_HOSTNAME)s'
-      $endpoint_public = 'False'
-      $endpoint_defaultdirectory = '/~/'
-      $security_fetchcredentialfromrelay = 'True'
-      $security_identitymethod = 'MyProxy'
-      $gridftp_server = '%(HOSTNAME)s'
-      $myproxy_server = '%(HOSTNAME)s'
+      $globus_user                          = '%(GLOBUS_USER)s'
+      $globus_password                      = '%(GLOBUS_PASSWORD)s'
+      $endpoint_name                        = '%(SHORT_HOSTNAME)s'
+      $endpoint_defaultdirectory            = '/~/'
+      $endpoint_public                      = 'False'
+      $security_authorizationmethod         = undef
+      $security_certificatefile             = undef
+      $security_cilogonidentityprovider     = undef
+      $security_fetchcredentialfromrelay    = 'True'
+      $security_gridmap                     = undef
+      $security_identitymethod              = 'MyProxy'
+      $security_keyfile                     = undef
+      $security_trustedcertificatedirectory = undef
+      $gridftp_datainterface                = undef
+      $gridftp_incomingportrange            = undef
+      $gridftp_outgoingportrange            = undef
+      $gridftp_restrictpaths                = undef
+      $gridftp_server                       = '%(HOSTNAME)s'
+      $gridftp_serverbehindnat              = undef
+      $gridftp_sharing                      = undef
+      $gridftp_sharinggroupsallow           = undef
+      $gridftp_sharinggroupsdeny            = undef
+      $gridftp_sharingrestrictpaths         = undef
+      $gridftp_sharingstatedir              = undef
+      $gridftp_sharingusersallow            = undef
+      $gridftp_sharingusersdeny             = undef
+      $myproxy_cadirectory                  = undef
+      $myproxy_configfile                   = undef
+      $myproxy_server                       = '%(HOSTNAME)s'
+      $myproxy_serverbehindnat              = undef
+      $oauth_logo                           = undef
+      $oauth_server                         = undef
+      $oauth_serverbehindnat                = undef
     }
 
     default: {
